dd79aa36906446e2f62285dc2f4b06f717fa9804
zhaoxiong0211 <zhaoxiong2015@u.northwestern.edu>
2016-01-21 22:26:59 -0600

    week1 blog

---
+++ week1/routes.js
@@ -0,0 +1,12 @@
+Router.configure({
+  layoutTemplate: 'layout'
+});
+Router.route('/', function () {
+  this.render('home');
+});
+Router.route('/projects', function () {
+  this.render('projects');
+});
+Router.route('/news', function () {
+  this.render('news');
+});
